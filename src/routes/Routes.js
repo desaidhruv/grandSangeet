7450cc73a18073ad59373e3932ae3c971919114b
kkeval <kevalpanchal58@gmail.com>
2021-09-28 13:19:30 +0530

    Project Structure

---
+++ src/routes/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+export default function Routes() {
+    return (
+        <>
+        <Router>
+          <Switch>
+          </Switch>
+        </Router>
+            
+        </>
+    )
+}
