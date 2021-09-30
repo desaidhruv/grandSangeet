343d610e20d7bc93833aa5192e2eadfc31a4d045
Dhruv Desai <dhruvshah28@gmail.com>
2021-10-01 00:32:47 +0530

    Team Updated

---
+++ src/components/sections/Team.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { VStack, Image, Button, Text, Center, HStack } from '@chakra-ui/react';
+import Rohan from '../../assets/photos/Rohan.png'
+
+function Team() {
+    return (
+        <VStack
+            as={Center}
+            h="600px"
+            spacing="35px"
+        >
+            <Text fontSize="48px" fontWeight="bold">Meet Our Team</Text>
+            <HStack spacing={'40px'} direction={["column", "row"]}>
+                <VStack>
+                        <Image
+                            src={Rohan}
+                        />
+                    <Text fontSize="24px">Rohan Pharek</Text>
+                </VStack>
+                <VStack>
+                        <Image
+                            src={Rohan}
+                        />
+                    <Text fontSize="24px" >Rohan Pharek</Text>
+                </VStack>
+                <VStack>
+                        <Image
+                            src={Rohan}
+                        />
+                    <Text fontSize="24px" >Rohan Pharek</Text>
+                </VStack>
+                <VStack>
+                        <Image
+                            src={Rohan}
+                        />
+                    <Text fontSize="24px" >Rohan Pharek</Text>
+                </VStack>
+            </HStack>
+        </VStack>
+
+    )
+}
+
+export default Team
