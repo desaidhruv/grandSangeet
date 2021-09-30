f83ac2bccd1d4d43d33997990920d09c51328d61
Dhruv Desai <dhruvshah28@gmail.com>
2021-10-01 00:33:21 +0530

    All Pages Added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Explore from "../components/sections/Explore"
 import Hero from '../components/sections/Hero'
+import Services from '../components/sections/Services'
+import About from '../components/sections/About'
+import Team from '../components/sections/Team'
+import Community from '../components/sections/Community'
 
 export default function Landing() {
     return (
         <>
-        <Hero/>
-        <Explore/>
-            
+        <Hero />
+        <Explore />
+        <Services />
+        <About />
+        <Team />
+        <Community />
+        
         </>
     )
 }
