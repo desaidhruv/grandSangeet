24700f65f8209c682ebeb8e199cbe2aee2f90af6
Dhruv Desai <dhruvshah28@gmail.com>
2021-10-01 23:28:07 +0530

    Footer and WhyWeAre Added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,18 +7,22 @@ import Team from '../components/sections/Team'
 import Community from '../components/sections/Community'
 import Gallery from '../components/sections/Gallery'
 import Contact from '../components/sections/Contact'
+import WhyWeAre from '../components/sections/WhyWeAre'
+import Footer from '../components/sections/Footer'
 
 export default function Landing() {
     return (
         <>
         <Hero />
         {/* <Explore /> */}
+        <WhyWeAre />
         <Services />
         <Gallery />
         <About />
         <Team />
         <Community />
-        <Contact/>
+        <Contact />
+        <Footer />
         
         </>
     )
