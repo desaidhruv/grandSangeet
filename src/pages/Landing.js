c2fbdf287f7d934a2b57b0bb500d92cc3c1b7ace
Dhruv Desai <dhruvshah28@gmail.com>
2021-10-01 16:12:48 +0530

    Gallery Updated

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,6 +5,7 @@ import Services from '../components/sections/Services'
 import About from '../components/sections/About'
 import Team from '../components/sections/Team'
 import Community from '../components/sections/Community'
+import Gallery from '../components/sections/Gallery'
 
 export default function Landing() {
     return (
@@ -15,6 +16,7 @@ export default function Landing() {
         <About />
         <Team />
         <Community />
+        <Gallery />
         
         </>
     )
