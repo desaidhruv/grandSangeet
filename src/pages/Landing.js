44e26b9c9534ba924727e87b37d92c7d70b6c35e
kkeval <kevalpanchal58@gmail.com>
2021-10-01 16:19:01 +0530

    Changes- Slider .js package added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import Explore from "../components/sections/Explore"
-import Hero from '../components/sections/Hero'
-import Services from '../components/sections/Services'
-import About from '../components/sections/About'
-import Team from '../components/sections/Team'
-import Community from '../components/sections/Community'
+import React from 'react';
+import Explore from '../components/sections/Explore';
+import Hero from '../components/sections/Hero';
+import Services from '../components/sections/Services';
+import About from '../components/sections/About';
+import Team from '../components/sections/Team';
+import Community from '../components/sections/Community';
 
 export default function Landing() {
-    return (
-        <>
-        <Hero />
-        <Explore />
-        <Services />
-        <About />
-        <Team />
-        <Community />
-        
-        </>
-    )
+  return (
+    <>
+      <Hero />
+      <Services />
+      <About />
+      <Team />
+      <Community />
+    </>
+  );
 }
