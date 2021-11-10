7ca28abdbecb944bb705407754eb74acf440f581
Dhruv Desai <dhruvshah28@gmail.com>
2021-11-10 10:27:10 +0530

    Loader Updated

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ import "./App.css"
 
 function App() {
   const [isLoading, setIsLoading] = React.useState(true);
-  const handleLoading = () => {
-    setIsLoading(false);
-  }
+  // const handleLoading = () => {
+  //   setIsLoading(false);
+  // }
   React.useEffect(() => {
-    window.addEventListener("load", handleLoading);
-    return () => window.removeEventListener("load", handleLoading);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000)
+    // window.addEventListener("load", handleLoading);
+    // return () => window.removeEventListener("load", handleLoading);
   }, [])
-  return !isLoading ? (
+  return(
     <ChakraProvider theme={theme}>
     {/* <Fonts /> */}
-    <Landing />
+    {isLoading ? <Loading /> : <Landing />}
+    
   </ChakraProvider>
-  ):(<Loading />)
+  ) 
+  // return isLoading ? (
+  //   <ChakraProvider theme={theme}>
+  //   {/* <Fonts /> */}
+  //   <Landing />
+  // </ChakraProvider>
+  // ):(<Loading />)
   // (
   //   <ChakraProvider theme={theme}>
   //     {/* <Fonts /> */}
